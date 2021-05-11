983f70510fe7d3e50d6df25fad9b5e7faea95e26
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-05-11 14:10:36 +0800

    feat: setup postgresql database

--- src/main/java/com/example/studentapi/student/Student.java
+++ src/main/java/com/example/studentapi/student/Student.java
@@ -1,8 +1,23 @@
 package com.example.studentapi.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+    // Map Student Class to Table
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize =  1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private String email;
