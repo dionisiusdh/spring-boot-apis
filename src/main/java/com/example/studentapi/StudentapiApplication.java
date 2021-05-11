18050a79c8b61e82d3657d388e3674aa428f103c
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-05-11 11:57:32 +0800

    feat: add Student class

--- src/main/java/com/example/studentapi/StudentapiApplication.java
+++ src/main/java/com/example/studentapi/StudentapiApplication.java
@@ -1,13 +1,33 @@
 package com.example.studentapi;
 
+import com.example.studentapi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class StudentapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentapiApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Bob",
+						"bob.123@gmail.com",
+						 LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
+	}
 }
