8b9c9a10cdae0ef6cc97302377d01e2975b097a4
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-05-11 11:36:14 +0800

    init: reinitialize spring maven project

--- src/test/java/com/example/studentapi/StudentapiApplicationTests.java
+++ src/test/java/com/example/studentapi/StudentapiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.studentapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class StudentapiApplicationTests {
 
 	@Test
 	void contextLoads() {
